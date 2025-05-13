58a9a8ac0f94717636c40829bf8aad5ac11ba267
CodeWithAmmar1 <m.aliammar.naseer@gmail.com>
2025-05-13 16:52:02 +0500

    Updating ui

--- lib/main.dart
+++ lib/main.dart
@@ -5,23 +5,37 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(BlocProvider(
-    create: (_) => TodoBloc(),
-    child: MaterialApp(home: TodoApp()),
-  ));
+  runApp(MyApp());
 }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+    create: (_) => TodoBloc(),
+    child: MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TodoApp()),
+  );
+  }
+}
 class TodoApp extends StatelessWidget {
   final TextEditingController controller = TextEditingController();
+
+  TodoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     final todoBloc = context.read<TodoBloc>();
 
     return Scaffold(
-      appBar: AppBar(title: Text('Simple Todo BLoC')),
+      appBar: AppBar(
+        backgroundColor: Colors.limeAccent,
+        title: Text('Simple Todo BLoC',style: TextStyle(fontWeight: FontWeight.bold,),),centerTitle: true,),
       body: Column(
         children: [
+          SizedBox(height: 20,),
           Padding(
             padding: EdgeInsets.all(12),
             child: Row(
@@ -32,14 +46,20 @@ class TodoApp extends StatelessWidget {
                     decoration: InputDecoration(hintText: 'Enter a task'),
                   ),
                 ),
-                IconButton(
-                  icon: Icon(Icons.add),
-                  onPressed: () {
-                    if (controller.text.trim().isNotEmpty) {
-                      todoBloc.add(AddTodo(controller.text.trim()));
-                      controller.clear();
-                    }
-                  },
+                Container(
+                decoration:BoxDecoration(
+                  color: Colors.limeAccent,shape: BoxShape.circle
+
+                ) ,
+                  child: IconButton(
+                    icon: Icon(Icons.add),
+                    onPressed: () {
+                      if (controller.text.trim().isNotEmpty) {
+                        todoBloc.add(AddTodo(controller.text.trim()));
+                        controller.clear();
+                      }
+                    },
+                  ),
                 ),
               ],
             ),
